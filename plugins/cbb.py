7f46d759c2eabe80bf1eeb22631d4d4e50c462fe
Lazzy Boy <147271056+lazzyboybots@users.noreply.github.com>
2024-11-23 11:36:01 +0530

    Update cbb.py

--- plugins/cbb.py
+++ plugins/cbb.py
@@ -10,7 +10,7 @@ async def cb_handler(client: Bot, query: CallbackQuery):
     data = query.data
     if data == "about":
         await query.message.edit_text(
-            text = f"<b>👨‍💻 Developer : <a href='tg://Itz_Sung_Jin_Woo'>This Person</a>\n📑 Language : <a href='https://python.org/'>Python3</a>\n📚 Library : <a href='https://docs.pyrogram.org/'>Pyrogram asyncio {__version__}</a>\n🌐 Server : <a href='https://heroku.com/'>Heroku</a>\n📣 Channel : <a href='https://t.me/Anime_Worlds_In_Tamil'>Anime World in Tamil<a/>\n🤖 My Name : <a href='https://t.me/Shonobi_File_Bot'>File Sharing Bot</a></b>",
+            text = f"<b>👨‍💻 Developer : <a href='tg://Itz_Sung_Jin_Woo'>This Person</a>\n📑 Language : <a href='https://python.org/'>Python3</a>\n📚 Library : <a href='https://docs.pyrogram.org/'>Pyrogram asyncio {__version__}</a>\n🌐 Server : <a href='https://heroku.com/'>Heroku</a>\n📣 Channel : <a href='https://t.me/Anime_Worlds_In_Tamil'>Anime World in Tamil</a>\n🤖 My Name : <a href='https://t.me/Shonobi_File_Bot'>File Sharing Bot</a></b>",
             disable_web_page_preview = True,
             reply_markup = InlineKeyboardMarkup(
                 [
